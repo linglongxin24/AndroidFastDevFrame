24e22ed21dae598e162a113f68a96ac2ae7d4bcb
linglongxin24 <linglongxin24@163.com>
2017-03-27 15:25:00 +0800

    添加了httputils中链式调用请求接口

--- httputils/src/main/java/cn/bluemobi/dylan/httputils/HttpUtils.java
+++ httputils/src/main/java/cn/bluemobi/dylan/httputils/HttpUtils.java
@@ -371,6 +371,148 @@ public class HttpUtils {
         return post(context, true, mapObservable, httpResponse);
     }
 
+    /**
+     * 要显示的对话框的上下文
+     */
+    private Context context;
+
+    /**
+     * 默认显示家在进度对话框
+     */
+    private boolean isShowLoadingDialog = true;
+    /**
+     * 接口对口
+     */
+    private Observable<ResponseBody> observable;
+
+    /**
+     * 【第一步】设置上下文
+     *
+     * @param context 上下文
+     * @return 本类对象
+     */
+    public HttpUtils with(Context context) {
+        this.context = context;
+        return this;
+    }
+
+    /**
+     * 【第二步】设置隐藏加载对话框（可忽略此步骤，默认显示）
+     *
+     * @return 本类对象
+     */
+    public HttpUtils hideLoadingDialog() {
+        isShowLoadingDialog = false;
+        return this;
+    }
+
+    /**
+     * 【第三步】设置访问的几口接口
+     *
+     * @param observable 接口对象
+     * @return 本类对象
+     */
+    public HttpUtils setObservable(Observable<ResponseBody> observable) {
+        this.observable = observable;
+        return this;
+    }
+
+    /**
+     * 【第四步】设置访问接口的返回监听
+     *
+     * @param httpResponse 请求相应监听
+     * @return 本类对象
+     */
+    public Subscription setDataListener(final HttpResponse httpResponse) {
+        String network_unusual = useEnglishLanguage ? "Network  unusual" : "网络不可用";
+        final String network_error = useEnglishLanguage ? "Network  error" : "网络繁忙";
+        if (context == null) {
+            throw new RuntimeException("请设置上下文对象--调用【with(Context context)】方法设置");
+        }
+        if (!NetworkUtil.isNetworkAvailable(context)) {
+            Toast.makeText(context, network_unusual, Toast.LENGTH_SHORT).show();
+            httpResponse.netOnFailure(new Exception(network_unusual));
+            httpResponse.netOnFinish();
+            return null;
+        }
+
+        LoadingDialog loadingDialog = null;
+        if (isShowLoadingDialog) {
+            if (loadingDialog == null) {
+                loadingDialog = new LoadingDialog(context);
+            }
+        } else {
+            loadingDialog = null;
+        }
+        if (observable == null) {
+            throw new RuntimeException("请设置要调用的接口对象--调用【setObservable(Observable<ResponseBody> observable)】方法设置");
+        }
+        final LoadingDialog finalLoadingDialog = loadingDialog;
+        Subscription subscribe = observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<ResponseBody>() {
+
+                    @Override
+                    public void onStart() {
+                        if (finalLoadingDialog != null) {
+                            finalLoadingDialog.show("");
+                        }
+                        httpResponse.netOnStart();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        if (finalLoadingDialog != null) {
+                            finalLoadingDialog.dismiss();
+                        }
+                        httpResponse.netOnFinish();
+                        context = null;
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        Toast.makeText(context, network_error, Toast.LENGTH_SHORT).show();
+                        httpResponse.netOnFailure(e);
+                        onCompleted();
+
+                    }
+
+                    @Override
+                    public void onNext(ResponseBody result) {
+                        ArrayMap<String, Object> jsonBean;
+                        try {
+                            jsonBean = jsonParse(result.string());
+                            String msg = getValue(jsonBean, HttpUtils.getInstance().getMsg());
+                            int code = Integer.parseInt(getValue(jsonBean, HttpUtils.getInstance().getCode()));
+
+                            if (showMessageModel == MessageModel.All) {
+                                if (msg != null && !msg.isEmpty()) {
+                                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            if (code == HttpUtils.getInstance().getSuccessCode()) {
+                                Map<String, Object> data = (Map<String, Object>) jsonBean.get(HttpUtils.getInstance().getData());
+                                httpResponse.netOnSuccess(data);
+                            } else {
+                                if (showMessageModel == MessageModel.OTHER_STATUS) {
+                                    if (msg != null && !msg.isEmpty()) {
+                                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                httpResponse.netOnOtherStatus(code, msg);
+                            }
+                        } catch (Exception e) {
+                            Toast.makeText(context, network_error, Toast.LENGTH_SHORT).show();
+                            httpResponse.netOnFailure(e);
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        return subscribe;
+    }
+
     public static Subscription post(final Context context, final boolean isShowLoadingDialog, Observable<ResponseBody> mapObservable, final HttpResponse httpResponse) {
         String network_unusual = useEnglishLanguage ? "Network  unusual" : "网络不可用";
         final String network_error = useEnglishLanguage ? "Network  error" : "网络繁忙";
@@ -456,6 +598,7 @@ public class HttpUtils {
                 });
         return subscribe;
     }
+
 
     private static ArrayMap<String, Object> jsonParse(String json) throws JSONException {
         ArrayMap<String, Object> arrayMap = JSON.parseObject(json, new TypeReference<ArrayMap<String, Object>>() {
