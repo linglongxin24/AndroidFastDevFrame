22afdf9c3512937e3413261b3e3f7e924c0172b4
linglongxin24 <linglongxin24@163.com>
2017-04-12 15:51:48 +0800

    修改了快速开发框架的loading

--- fastdev/src/main/java/cn/bluemobi/dylan/fastdev/http/HttpCallBack.java
+++ fastdev/src/main/java/cn/bluemobi/dylan/fastdev/http/HttpCallBack.java
@@ -61,7 +61,7 @@ public class HttpCallBack implements Callback.ProgressCallback<String> {
     @Override
     public void onStarted() {
         if (loadingDialog != null) {
-            loadingDialog.show("Loading...");
+            loadingDialog.show("");
         }
 
         httpResponse.netOnStart();
