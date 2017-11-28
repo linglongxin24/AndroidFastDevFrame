a240aff2779fa0825dc8bcafe0c1a4dcdf189c9f
linglongxin24 <linglongxin24@163.com>
2017-11-28 14:16:30 +0800

    添加侵入式状态栏

---
+++ app/src/main/java/com/dylanfastdev/TextActivity.java
@@ -0,0 +1,42 @@
+package com.dylanfastdev;
+
+import android.os.Bundle;
+import android.view.View;
+
+import cn.bluemobi.dylan.base.BaseActivity;
+
+/**
+ * Created by lenovo on 2017/11/28.
+ */
+
+public class TextActivity extends BaseActivity {
+    @Override
+    public void initTitleBar() {
+        setTitle("测试");
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void addListener() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
