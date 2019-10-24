82bf3e243c6a52885a066ebc4f26f3149d291dbb
linglongxin24 <linglongxin24@163.com>
2019-10-24 10:52:58 +0800

    fixs bugs

---
+++ app/src/main/java/com/dylanfastdev/DialogBugActivity.java
@@ -0,0 +1,50 @@
+package com.dylanfastdev;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import cn.bluemobi.dylan.base.BaseActivity;
+import cn.bluemobi.dylan.base.view.iOSOneButtonDialog;
+
+/**
+ * @author dylan
+ * @date 2019/10/24
+ */
+public class DialogBugActivity extends BaseActivity {
+    @Override
+    public void initTitleBar() {
+        setTitle("测试三星手机后台弹框bug");
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                new iOSOneButtonDialog(mContext).setMessage("测试弹框有没有出来").show();
+            }
+        }, 30 * 1000);
+
+    }
+
+    @Override
+    public void addListener() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
