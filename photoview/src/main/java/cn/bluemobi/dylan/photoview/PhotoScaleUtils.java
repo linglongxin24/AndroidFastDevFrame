682fe20a3be15bfb88f01c2dd3413081bfa9276e
linglongxin24 <linglongxin24@163.com>
2020-07-23 17:59:00 +0800

    修改默认图片浏览显示保存按钮的bug

--- photoview/src/main/java/cn/bluemobi/dylan/photoview/PhotoScaleUtils.java
+++ photoview/src/main/java/cn/bluemobi/dylan/photoview/PhotoScaleUtils.java
@@ -22,7 +22,18 @@ public class PhotoScaleUtils {
     public static void scale(Context mContext, String url) {
         List<String> list = new ArrayList<String>();
         list.add(url);
-        scale(mContext, list, 0);
+        scale(mContext, list);
+    }
+    /**
+     * 浏览单张图片
+     *
+     * @param mContext 上下文
+     * @param url      图片地址
+     */
+    public static void scale(Context mContext, String url, boolean isShowSaveButton) {
+        List<String> list = new ArrayList<String>();
+        list.add(url);
+        scale(mContext, list, 0,isShowSaveButton);
     }
 
     /**
@@ -32,22 +43,44 @@ public class PhotoScaleUtils {
      * @param paths    图片地址列表
      */
     public static void scale(Context mContext, List<String> paths) {
+        scale(mContext, paths, 0,false);
+    }
+
+
+    /**
+     * 浏览多张图片
+     *
+     * @param mContext 上下文
+     * @param paths    图片地址列表
+     */
+    public static void scale(Context mContext, List<String> paths, boolean isShowSaveButton) {
         scale(mContext, paths, 0);
+    }
+    /**
+     * 浏览多张图片的指定位置图片
+     *
+     * @param mContext 上下文
+     * @param paths    图片地址集合
+     * @param position 位置
+     */
+    public static void scale(Context mContext, List<String> paths, int position) {
+        scale(mContext, paths, position, false);
     }
 
     /**
      * 浏览多张图片的指定位置图片
+     *
      * @param mContext 上下文
-     * @param paths 图片地址集合
+     * @param paths    图片地址集合
      * @param position 位置
      */
-    public static void scale(Context mContext, List<String> paths, int position) {
+    public static void scale(Context mContext, List<String> paths, int position, boolean isShowSaveButton) {
         Intent intent = new Intent(mContext, ImagePagerActivity.class);
         // 图片url,为了演示这里使用常量，一般从数据库中或网络中获取
 
         String[] arr = new String[paths.size()];
         arr = paths.toArray(arr);
-        intent.putExtra(ImagePagerActivity.EXTRA_IS_SHOW_SAVE_BUTTON, true);
+        intent.putExtra(ImagePagerActivity.EXTRA_IS_SHOW_SAVE_BUTTON, isShowSaveButton);
         intent.putExtra(ImagePagerActivity.EXTRA_IMAGE_URLS, arr);
         intent.putExtra(ImagePagerActivity.EXTRA_IMAGE_INDEX, position);
         mContext.startActivity(intent);
