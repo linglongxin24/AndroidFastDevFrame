01d9f18307ffc483732c3a630f42e21ee582f456
linglongxin24 <linglongxin24@163.com>
2021-01-14 15:49:01 +0800

    Revert "http网络请求修改为kotlin失败"
    This reverts commit bf88911e

--- http/src/main/java/cn/bluemobi/dylan/http/download/ProgressResponseListener.kt
+++
@@ -1,9 +0,0 @@
-package cn.bluemobi.dylan.http.download
-
-/**
- * Created by YDL on 2017/6/8./ **
- * 响应体进度回调接口，用于文件下载进度回调
- */
-interface ProgressResponseListener {
-    fun onResponseProgress(bytesRead: Long, contentLength: Long, done: Boolean)
-}
